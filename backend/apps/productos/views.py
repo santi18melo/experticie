44c9a64b2095ddbb51d717b9f3be9ab35d4ae541
santi18melo <melosanchezsantiago@gmail.com>
2025-12-05 09:39:22 -0500

    Add CanDeleteProduct permission to enforce proper delete restrictions

--- backend/apps/productos/views.py
+++ backend/apps/productos/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework import viewsets, status, permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, BasePermission
 from django.db import transaction
 
 
